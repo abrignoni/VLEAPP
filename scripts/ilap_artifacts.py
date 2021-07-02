54c200d1b286518757f56419b35be04ac4d65ecb
Alexis Bringoni <abrignoni@gmail.com>
2021-07-01 22:37:17 -0400

    Initial commit of codebase
    No VLEAPP specific artifacts yet.
    Currently only one Android placeholder artifact.

---
+++ scripts/ilap_artifacts.py
@@ -0,0 +1,63 @@
+# To add a new artifact module, import it here as shown below:
+#     from scripts.artifacts.fruitninja import get_fruitninja
+# Also add the grep search for that module using the same name
+# to the 'tosearch' data structure.
+
+import traceback
+
+from scripts.artifacts.usagestatsVersion import get_usagestatsVersion
+
+from scripts.ilapfuncs import *
+
+# GREP searches for each module
+# Format is Key='modulename', Value=Tuple('Module Pretty Name', 'regex_term')
+#   regex_term can be a string or a list/tuple of strings
+# Here modulename must match the get_xxxxxx function name for that module. 
+# For example: If modulename='profit', function name must be get_profit(..)
+# Don't forget to import the module above!!!!
+
+tosearch = {
+    'usagestatsVersion':('Usage Stats', ('*/system/usagestats/*/version', '*/system_ce/*/usagestats/version')),
+    }
+
+slash = '\\' if is_platform_windows() else '/'
+
+def process_artifact(files_found, artifact_func, artifact_name, seeker, report_folder_base, wrap_text):
+    ''' Perform the common setup for each artifact, ie, 
+        1. Create the report folder for it
+        2. Fetch the method (function) and call it
+        3. Wrap processing function in a try..except block
+
+        Args:
+            files_found: list of files that matched regex
+
+            artifact_func: method to call
+
+            artifact_name: Pretty name of artifact
+
+            seeker: FileSeeker object to pass to method
+            
+            wrap_text: whether the text data will be wrapped or not using textwrap.  Useful for tools that want to parse the data.
+    '''
+    logfunc('{} [{}] artifact executing'.format(artifact_name, artifact_func))
+    report_folder = os.path.join(report_folder_base, artifact_name) + slash
+    try:
+        if os.path.isdir(report_folder):
+            pass
+        else:
+            os.makedirs(report_folder)
+    except Exception as ex:
+        logfunc('Error creating {} report directory at path {}'.format(artifact_name, report_folder))
+        logfunc('Reading {} artifact failed!'.format(artifact_name))
+        logfunc('Error was {}'.format(str(ex)))
+        return
+    try:
+        method = globals()['get_' + artifact_func]
+        method(files_found, report_folder, seeker, wrap_text)
+    except Exception as ex:
+        logfunc('Reading {} artifact had errors!'.format(artifact_name))
+        logfunc('Error was {}'.format(str(ex)))
+        logfunc('Exception Traceback: {}'.format(traceback.format_exc()))
+        return
+
+    logfunc('{} [{}] artifact completed'.format(artifact_name, artifact_func))
---
+++ scripts/version_info.py
@@ -0,0 +1,10 @@
+vleapp_version = '0.0.1'
+
+# Contributors List
+# Format = [ Name, Blog-url, Twitter-handle, Github-url]
+# Leave blank if not available
+vleapp_contributors = [
+    [ 'Alexis Brignoni', 'https://abrignoni.com', '@AlexisBrignoni', 'https://github.com/abrignoni'],
+    [ 'Geraldine Blay', 'https://gforce4n6.blogpost.com', '@i_am_the_gia', ''],
+    [ 'Yogesh Khatri', 'https://swiftforensics.com', '@SwiftForensics', 'https://github.com/ydkhatri'],
+]
