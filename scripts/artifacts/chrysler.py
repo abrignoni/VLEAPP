47691cd2ef9cb9e1a147bd36181571ba119c9a99
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-26 11:50:34 -0400

    adding framework for 3 distinct artifacts
    Co-authored-by: Phoenix (Nixy) Camacho <phoenixrising1800@users.noreply.github.com>

--- scripts/artifacts/chrysler.py
+++ scripts/artifacts/chrysler.py
@@ -8,36 +8,88 @@ from scripts.ilapfuncs import logfunc, tsv, logdevinfo, is_platform_windows
 vehicles = ['FCA','Jeep Cherokee']
 platforms = ['Carplay']
 
-def get_btDevices():
-    print("nothing")
+## Get connected Bluetooth Devices
+def get_btDevices(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    for file_found in files_found:
+        with open(file_found, "r") as f:
+            for line in f:
+                if len(data_list) > 0:
+                    report = ArtifactHtmlReport('Vehicle Info')
+                    report.start_artifact_report(report_folder, f'Vehicle Info')
+                    report.add_script()
+                    data_headers = ('Key','Value')
+                    report.write_artifact_data_table(data_headers, data_list, file_found)
+                    report.end_artifact_report()
+        
+                    tsvname = f'Vehicle Info'
+                    tsv(report_folder, data_headers, data_list, tsvname)
+        
+                else:
+                    logfunc(f'No Vehicle Info available')
 
-def get_contacts():
-    print("nothing")
+## Get known contacts
+def get_contacts(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    for file_found in files_found:
+        with open(file_found, "r") as f:
+            if len(data_list) > 0:
+                report = ArtifactHtmlReport('Vehicle Info')
+                report.start_artifact_report(report_folder, f'Vehicle Info')
+                report.add_script()
+                data_headers = ('Key','Value')
+                report.write_artifact_data_table(data_headers, data_list, file_found)
+                report.end_artifact_report()
+            
+                tsvname = f'Vehicle Info'
+                tsv(report_folder, data_headers, data_list, tsvname)
+        
+            else:
+                logfunc(f'No Vehicle Info available')
 
-def get_calllogs():
-    print("nothing")
+## Get recent calls
+#def get_calllogs(files_found, report_folder, seeker, wrap_text):
+#   print("nothing")
 
-def get_gpsdata():
-    print("nothing")
+## Get GPS data
+def get_gpsdata(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+
+
+
+
+    if len(data_list) > 0:
+        report = ArtifactHtmlReport('Vehicle Info')
+        report.start_artifact_report(report_folder, f'Vehicle Info')
+        report.add_script()
+        data_headers = ('Key','Value')
+        report.write_artifact_data_table(data_headers, data_list, file_found)
+        report.end_artifact_report()
+        
+        tsvname = f'Vehicle Info'
+        tsv(report_folder, data_headers, data_list, tsvname)
+        
+    else:
+        logfunc(f'No Vehicle Info available')
 
 
 
 __artifacts__ = {
     "bluetooth_devices": (
         "bluetooth devices",
-        ('*/com.android.cooldata/databases/database*.db'),
+        ('*/mnt/p3/betula/bt_log.txt'),
         get_btDevices),
     "contacts": (
         "contacts",
-        ('*/com.android.cooldata/files/cool.xml'),
+        ('*/mnt/p3/voice/asr/context/phonebook/*.txt'),
         get_contacts),
-    "call_logs": (
-        "call_logs",
-        ('*/com.android.cooldata/files/cool.xml'),
-        get_calllogs),
+   # "call_logs": (
+     #  "call_logs",
+      #  ('*/com.android.cooldata/files/cool.xml'),
+     #   get_calllogs),
     "gps_data": (
         "gps_data",
-        ('dir'),
+        ('*/mnt/p3/persistence/nonvol_*.ps'),
         get_gpsdata)
 
     
