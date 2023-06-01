d39b1747c9cbd80f61670f9264fca44371bbc942
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-06-01 10:45:48 -0400

    double bracket Co-authored-by: Phoenix (Nixy) Camacho <phoenixrising1800@users.noreply.github.com>

--- scripts/artifacts/chrysler.py
+++ scripts/artifacts/chrysler.py
@@ -77,7 +77,7 @@ def get_diagnosticdata(files_found, report_folder, seeker, wrap_text):
                 if "AA_VEHICLE_ID" in line:
                     splits = line.split('AA_VEHICLE_ID')
                     key = splits[1]
-            data_list.append(key)             
+            data_list.append((key))             
     if len(data_list) > 0:
         report = ArtifactHtmlReport('Diagnostic Data')
         report.start_artifact_report(report_folder, f'Diagnostic Data')
