144739963531c4d9b34d8aba2455490222737ad9
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-06-06 12:03:52 -0400

    better

--- scripts/artifacts/chrysler_contacts.py
+++ scripts/artifacts/chrysler_contacts.py
@@ -45,8 +45,8 @@ def get_contacts(files_found, report_folder, seeker, wrap_text):
 
 
         __artifacts__ = {
-       "contacts_list": (
-        "contacts",
+       "contactList": (
+        "contactsList",
         ('*/mnt/p3/voice/asr/context/phonebook/*.txt'),
         get_contacts)
 }
