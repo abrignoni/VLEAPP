363f04fe9b0bfa9e9a3b8a37740395de0279936b
Nixy Camacho <phoenixrising1800@gmail.com>
2023-06-09 08:14:44 -0400

    fix gps

--- scripts/artifacts/chrysler_gps.py
+++ scripts/artifacts/chrysler_gps.py
@@ -18,14 +18,13 @@ def get_gpsdata(files_found, report_folder, seeker, wrap_text):
     for file_found in files_found:
         try:
             with open(file_found, "r") as f:
-                while(data_list == ''):
-                        for line in f:
-                            line_str = str(line)
-                            line_str_decoded = bytes(line_str, "utf-8").decode("unicode_escape", errors="replace")
-                            line_decoded = re.sub('r\\\\x[0-9a-fA-F]{2}', "", line_str_decoded)
-                            line_wanted = line_decoded.encode('ascii', 'ignore').decode('ascii', errors="replace") 
-                            if line_wanted.contains("Latitude"):
-                                data_list.append((line_wanted))
+                for line in f:
+                    line_str = str(line)
+                    line_str_decoded = bytes(line_str, "utf-8").decode("unicode_escape", errors="replace")
+                    line_decoded = re.sub('r\\\\x[0-9a-fA-F]{2}', "", line_str_decoded)
+                    line_wanted = line_decoded.encode('ascii', 'ignore').decode('ascii', errors="replace") 
+                    if line_wanted.contains("Latitude"):
+                        data_list.append((line_wanted))
         except PermissionError:
             print("directory is not writable")
 
