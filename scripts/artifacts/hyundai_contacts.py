5fae3e5eae844adbd4963c23b569e83b5e4b14d2
Nixy Camacho <phoenixrising1800@gmail.com>
2023-07-06 09:38:53 -0400

    asdf

--- scripts/artifacts/hyundai_contacts.py
+++ scripts/artifacts/hyundai_contacts.py
@@ -14,48 +14,22 @@ def get_contacts(files_found, report_folder, seeker, wrap_text):
     for file_found in files_found:
         db = open_sqlite_db_readonly(file_found)
         cursor = db.cursor()
-        cursor.execute('''
-            SELECT
-            FROM
-            NAME
-            FROM SQLITE_SCHEMA
-            WHERE NAME LIKE ''
-            order by name
-            '''
-        )
-        all_rows = cursor.fetchall()
+        res = cursor.execute("SELECT name FROM sqlite_master WHERE name='bluetooth_contacts'")
+        all_rows = res.fetchall()
         usageentries = len(all_rows)
         if usageentries > 0:
             for row in all_rows:
                 data_list = []
-                data_list_as_is = []
                     
                 tablename = (row[0])
-                tablenamenum = tablename.split('_')[2]
+                #tablenamenum = tablename.split('_')[2]
+                res.execute("SELECT _id from bluetooth_contacts")
                     
-                cursor.execute(f''' 
-                    SELECT
-                    num_phonebook_{tablenamenum}.entry_id,
-                    phonebook_{tablenamenum}.first_name,
-                    phonebook_{tablenamenum}.last_name,
-                    GROUP_CONCAT(num_phonebook_{tablenamenum}.number, '; ')
-                    from num_phonebook_{tablenamenum}
-                    join phonebook_{tablenamenum} where num_phonebook_{tablenamenum}.entry_id = phonebook_{tablenamenum}.entry_id
-                    group by num_phonebook_{tablenamenum}.entry_id
-                    ''')
-                    
-                all_rows_inner = cursor.fetchall()
-                usageentries_inner = len(all_rows_inner)
-                    
-                if usageentries_inner > 0:
-                    for rows_inner in all_rows_inner:
-                        data_list.append((rows_inner[0], rows_inner[1], rows_inner[2], rows_inner[3].replace(';', '<br>')))
-                        data_list_as_is.append((rows_inner[0], rows_inner[1], rows_inner[2], rows_inner[3]))
     if len(data_list) > 0:
         report = ArtifactHtmlReport('Contact Data')
         report.start_artifact_report(report_folder, f'Contact Data')
         report.add_script()
-        data_headers = ('ID','Value')
+        data_headers = ('ID','given_name', 'family_name', 'phone_number', 'phone_type')
         report.write_artifact_data_table(data_headers, data_list, file_found)
         report.end_artifact_report()
         tsvname = f'Contact Data'
