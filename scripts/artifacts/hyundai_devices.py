c3a543036cb50b467f74f7d3b22e7ca3543dfdb5
Nixy Camacho <phoenixrising1800@gmail.com>
2023-07-28 13:52:30 -0400

    devices test 11

--- scripts/artifacts/hyundai_devices.py
+++ scripts/artifacts/hyundai_devices.py
@@ -54,8 +54,8 @@ def get_devices(files_found, report_folder, seeker, wrap_text):
                                 devAddr.append('')
                                 tup = (devAddr[i], devFriendlyName[i])
                             continue
+                        logfunc("TUPLE: " + str(tup))
                         data_list.append(tup)
-                        break
     if len(data_list) > 0:
         report = ArtifactHtmlReport('Bluetooth Devices')
         report.start_artifact_report(report_folder, f'Bluetooth Devices')
